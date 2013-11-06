1114da99e6006f8d1d8a3ebbfcc7d2caac2a7b46
madanMegusta <madanMegusta@megusta.ag>
2013-11-06 17:33:18 +0530

    Ajay added project code by  first commit

---
+++ llamrei/grails-app/views/show1/_dashBoard.gsp
@@ -0,0 +1,5 @@
+<div style="display: block;margin-top: 40px" id="dashBoardDiv">
+    <h3>Summary</h3>
+    <h3>Fuel</h3>
+    <h3>Power</h3>
+</div>
---
+++ llamrei/test/unit/untitled3/Show1ControllerTests.groovy
@@ -0,0 +1,17 @@
+package untitled3
+
+import grails.test.*
+
+class Show1ControllerTests extends ControllerUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
