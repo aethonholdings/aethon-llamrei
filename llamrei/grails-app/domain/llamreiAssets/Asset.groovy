d9dbcb4a46b3a4b3028a5a2f50194b5db0cee87d
Shweta Gupta <shwetag@jbilling.com>
2013-11-14 00:23:13 +0530

    Resolved compilation error

--- llamrei/grails-app/domain/llamreiAssets/Asset.groovy
+++ llamrei/grails-app/domain/llamreiAssets/Asset.groovy
@@ -2,6 +2,7 @@ package llamreiAssets
 
 import llamreiproject.User
 import llamreiproject.SecUser
+
 class Asset {
 
     /**
@@ -9,28 +10,22 @@ class Asset {
      * instance Variables
      */
     String assetUniqueID
-      String assetName
-      String clientName
-      String description
-      String location
-      String stateModelId
-      String imageurl
-      String imagethumburl
-      Date creationDate
-      Date modificationDate
+    String assetName
+    String clientName
+    String description
+    String location
+    String stateModelId
+    String imageurl
+    String imagethumburl
+    Date creationDate
+    Date modificationDate
 
-    String assetUniqueID
     String name
-    String Description
-    String Location
     String customer
     String Photo
 
 
     String deviceId
-    String description
-    Date creationDate
-    Date modificationDate
     SecUser createdBy
     SecUser modifiedBy
 
@@ -43,18 +38,18 @@ class Asset {
      * Putting constraints with properties
      */
     static constraints = {
-        assetUniqueID(nullable: true,unique: true)
+        assetUniqueID(nullable: true, unique: true)
         assetName(nullable: false)
         description(nullable: true)
         location(nullable: false)
-        stateModelId (nullable: false)
+        stateModelId(nullable: false)
         imageurl(nullable: false)
-        imagethumburl (nullable: false)
-        creationDate (nullable: true)
+        imagethumburl(nullable: false)
+        creationDate(nullable: true)
         modificationDate(nullable: true)
-        assetUniqueID(nullable: false,unique: true)
+        assetUniqueID(nullable: false, unique: true)
         name(nullable: false)
-        deviceId(nullable: false, blank: false,unique: true)
+        deviceId(nullable: false, blank: false, unique: true)
         creationDate(nullable: false, blank: false)
         modificationDate(nullable: false, blank: false)
         createdBy(nullable: false, blank: false)
@@ -65,13 +60,13 @@ class Asset {
      * Mapping to define column names for domain objects in the database
      */
     static mapping = {
-   assetUniqueID column:"assetUniqueID"
-        assetName column:"assetName"
-        stateModelId column:"stateModelId"
-        imageurl column : "imageurl"
-        imagethumburl column:"imagethumburl"
+        assetUniqueID column: "assetUniqueID"
+        assetName column: "assetName"
+        stateModelId column: "stateModelId"
+        imageurl column: "imageurl"
+        imagethumburl column: "imagethumburl"
         creationDate column: "creationDate"
-        modificationDate column:"modificationDate"
+        modificationDate column: "modificationDate"
         deviceId column: "deviceId"
         description column: "description"
         creationDate column: "creationDate"
