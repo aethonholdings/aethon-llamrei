031443e87f2acf83fcc486588cf3603898fb328b
Raj <rajp@damyant.com>
2013-11-25 20:03:51 +0530

    changed

--- llamrei/grails-app/domain/com/llamrei/domain/StateRule.groovy
+++ llamrei/grails-app/domain/com/llamrei/domain/StateRule.groovy
@@ -6,21 +6,20 @@ class StateRule {
     String ruleType
     TimeSeries timeSeries
     State state
-    String strRule
     String ruleValue1
     String ruleValue2
     static belongsTo = [state: State]
     
     static constraints = {
         stateRuleId(nullable: true)
-        strRule(nullable: false)
+
         ruleValue2(nullable: true)
     }
 
     static mapping = {
         stateRuleId column: "stateRuleId"
         ruleType column: "ruleType"
-        strRule column: "strRule"
+
         ruleValue1   column  : "ruleValue1"
         ruleValue2   column  : "ruleValue2"
     }
