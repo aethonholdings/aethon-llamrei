a3610694b426529370cce17b46537890e9e34429
Konstantinos Dimitriou <konstantinos.dimitriou@aethon.sg>
2013-11-26 20:21:12 +0800

    Removed debugging

--- llamrei/grails-app/controllers/com/llamrei/controllers/ListenerController.groovy
+++ llamrei/grails-app/controllers/com/llamrei/controllers/ListenerController.groovy
@@ -7,19 +7,28 @@ class ListenerController {
 
     def index = {
         
-        def thisAsset = Asset.findByAssetUniqueID(params?.id)
-        def thisNodeTimeStamp = new Date().parse("yyyy-mm-dd|hh:mm:ss", params?.getProperty("T"))
-        def thisServerTimeStamp = new Date()
-        
-        if(thisAsset!=null && thisNodeTimeStamp != null)
-        for (param in params) {
-            if(param?.key.length()==4) {
-                def thisTimeSeries = TimeSeries.findByTimeSeriesUniqueID(param.key)
-                if(thisTimeSeries!=null) {
-                    def thisDataPoint = new DataPoint(asset: thisAsset, timeSeries: thisTimeSeries, nodeTimestamp: thisNodeTimeStamp, timestamp: thisServerTimeStamp, value: param.value)
-                    thisDataPoint.save(flush: true)
+        if(params.id!=null) {
+            def thisAsset = Asset.findByAssetUniqueID(params.id)
+            if(thisAsset!=null) {
+                render("ACK")
+                def thisNodeTimeStamp = new Date().parse("yyyy|mm|dd|hh|mm|ss", params?.getProperty("T"))
+                def thisServerTimeStamp = new Date()
+                if(thisNodeTimeStamp != null) {
+                    for (param in params) {
+                        if(param?.key.length()==4) {
+                            def thisTimeSeries = TimeSeries.findByTimeSeriesUniqueID(param.key)
+                            if(thisTimeSeries!=null) {
+                                def thisDataPoint = new DataPoint(asset: thisAsset, timeSeries: thisTimeSeries, nodeTimestamp: thisNodeTimeStamp, timestamp: thisServerTimeStamp, value: param.value)
+                                thisDataPoint.save(flush: true)
+                            }
+                        }
+                    }
                 }
+            } else {
+                render("FORBIDDEN")    
             }
+        } else {
+            render("FORBIDDEN")
         }
-    }
+    }    
 }
